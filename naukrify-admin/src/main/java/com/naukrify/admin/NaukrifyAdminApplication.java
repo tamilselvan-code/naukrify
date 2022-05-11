6bf4badd7f626489343bb7370bc4c2f951f38758
Tamilselvan <tamilselvan.coder@gmail.com>
2022-05-11 16:14:14 +0530

    initial commit - spring boot

---
+++ naukrify-admin/src/main/java/com/naukrify/admin/NaukrifyAdminApplication.java
@@ -0,0 +1,13 @@
+package com.naukrify.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaukrifyAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaukrifyAdminApplication.class, args);
+	}
+
+}
---
+++ naukrify-admin/src/test/java/com/naukrify/admin/NaukrifyAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naukrify.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaukrifyAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
